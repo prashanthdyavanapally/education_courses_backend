4a9f37c5c1dabb1b3a233f273514480beeeacbe7
prashanthdyavanapally <dyavanapally.prashanth@capgemini.com>
2023-02-01 13:41:30 +0530

    connected to db using mongodb atlas

--- configs/db.js
+++ configs/db.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 const connect = () => {
-  return mongoose.connect(`mongodb://127.0.0.1:27017/education`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  return mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.azfufbj.mongodb.net/education?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 };
 
 module.exports = connect;
